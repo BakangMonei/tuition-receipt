2dd0b2f5f3caa484705d15594ed57560909584bf
BakangMonei <bakangmonei2@gmail.com>
2024-08-21 19:23:45 +0200

    ojo

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,10 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Add Auth
-import { collection, getFirestore } from "firebase/firestore"; // Add Firestore
+import { collection, getFirestore, query, where, getDocs  } from "firebase/firestore"; // Add Firestore
 import { getDatabase } from "firebase/database"; // Add Realtime Database
 import { getStorage } from "firebase/storage"; // Add Storage
-import { query, where, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -20,13 +19,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
 
 // Initialize Firebase services
 const auth = getAuth(firebaseApp); // Initialize Auth 
 const firestore = getFirestore(firebaseApp); // Initialize Firestore
 const database = getDatabase(firebaseApp); // Initialize Realtime Database
 const storage = getStorage(firebaseApp); // Initialize Storage
+const analytics = getAnalytics(firebaseApp); // Initialize Analytics
 
-
-export { auth, firestore, database, storage, firebaseApp };
+export { auth, firestore, database, storage, analytics, firebaseApp };
